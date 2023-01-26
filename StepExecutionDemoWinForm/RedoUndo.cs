f513f19a921443089a437d67cd169817a4e58cff
crimson-tea <91731135+crimson-tea@users.noreply.github.com>
2023-01-27 01:14:25 +0900

    fix: 謎の実装ミス

--- StepExecutionDemoWinForm/RedoUndo.cs
+++ StepExecutionDemoWinForm/RedoUndo.cs
@@ -1,5 +1,4 @@
-﻿using AnimationWinForm.Control3;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 
 namespace AnimationWinForm;
 
@@ -7,16 +6,23 @@ internal class RedoUndo<TOperation> where TOperation : class
 {
     private Stack<TOperation> _undo;
     private Stack<TOperation> _redo;
-    private Action<Operation>? executeRedo;
-    private Action<Operation>? executeUndo;
+    private Action<TOperation>? _executeRedo;
+    private Action<TOperation>? _executeUndo;
     private Action<int>? _setProgress;
 
+    /// <summary>
+    /// RedoUndoを実装します。
+    /// </summary>
+    /// <param name="redo">Redoで行われるアクションを指定します。</param>
+    /// <param name="undo">Undoで行われるアクションを指定します。</param>
+    /// <param name="setProgress">ステップ数を受け取りたい場合にアクションを指定します。</param>
+    /// <param name="initCapacity">RedoとUndoに使われるスタックの大きさを指定します。RedoとUndoそれぞれこのキャパシティで初期化されます。</param>
     public RedoUndo(Action<TOperation> redo, Action<TOperation> undo, Action<int> setProgress = null, int initCapacity = 65535)
     {
-        _undo = new Stack<TOperation>(initCapacity);
         _redo = new Stack<TOperation>(initCapacity);
-        UndoAction = undo;
-        RedoAction = redo;
+        _undo = new Stack<TOperation>(initCapacity);
+        _executeRedo = redo;
+        _executeUndo = undo;
         _setProgress = setProgress;
         InitCapacity = initCapacity;
     }
@@ -33,12 +39,12 @@ internal class RedoUndo<TOperation> where TOperation : class
     {
         _redo.Clear();
         _undo.Push(op);
-        RedoAction(op);
+        _executeRedo?.Invoke(op);
         _setProgress?.Invoke(_undo.Count);
     }
 
     /// <summary>
-    /// 
+    /// Redoを試します。
     /// </summary>
     /// <returns>true: 成功 false: 失敗</returns>
 
@@ -51,7 +57,7 @@ internal class RedoUndo<TOperation> where TOperation : class
 
         var op = _redo.Pop();
         _undo.Push(op);
-        RedoAction(op);
+        _executeRedo?.Invoke(op);
 
         _setProgress?.Invoke(_undo.Count);
         Debug.WriteLine(op.ToString());
@@ -59,7 +65,7 @@ internal class RedoUndo<TOperation> where TOperation : class
     }
 
     /// <summary>
-    /// 
+    /// Undoを試します。
     /// </summary>
     /// <returns>true: 成功 false: 失敗</returns>
     public bool Undo()
@@ -71,13 +77,16 @@ internal class RedoUndo<TOperation> where TOperation : class
 
         var op = _undo.Pop();
         _redo.Push(op);
-        UndoAction(op);
+        _executeUndo?.Invoke(op);
 
         _setProgress?.Invoke(_undo.Count);
         Debug.WriteLine(op.ToString());
         return true;
     }
 
+    /// <summary>
+    /// RedoUndoをリセットします。
+    /// </summary>
     internal void Reset()
     {
         _redo.Clear();

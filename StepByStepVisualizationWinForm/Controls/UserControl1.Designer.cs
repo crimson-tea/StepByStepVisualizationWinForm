5ebc1fdc081b496bf8b2f21ae21b7c27bd3b07c6
crimson-tea <91731135+crimson-tea@users.noreply.github.com>
2023-02-03 01:04:44 +0900

    update: よりRedoUndoができることが伝わりそうな実装に

--- StepByStepVisualizationWinForm/Controls/UserControl1.Designer.cs
+++ StepByStepVisualizationWinForm/Controls/UserControl1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace StepByStepVisualizationWinForm
+﻿namespace StepByStepVisualizationWinForm.Controls
 {
     partial class UserControl1
     {
@@ -28,61 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.PreviousButton = new System.Windows.Forms.Button();
+            this.ZeroButton = new System.Windows.Forms.Button();
+            this.OneButton = new System.Windows.Forms.Button();
+            this.DeleteButton = new System.Windows.Forms.Button();
             this.NextButton = new System.Windows.Forms.Button();
-            this.StartButton = new System.Windows.Forms.Button();
+            this.PreviousButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // PreviousButton
+            // ZeroButton
             // 
-            this.PreviousButton.Location = new System.Drawing.Point(3, 212);
-            this.PreviousButton.Name = "PreviousButton";
-            this.PreviousButton.Size = new System.Drawing.Size(75, 23);
-            this.PreviousButton.TabIndex = 9;
-            this.PreviousButton.Text = "Prev";
-            this.PreviousButton.UseVisualStyleBackColor = true;
-            this.PreviousButton.Click += new System.EventHandler(this.PreviousButton_Click);
+            this.ZeroButton.Location = new System.Drawing.Point(3, 65);
+            this.ZeroButton.Name = "ZeroButton";
+            this.ZeroButton.Size = new System.Drawing.Size(75, 23);
+            this.ZeroButton.TabIndex = 0;
+            this.ZeroButton.Text = "0";
+            this.ZeroButton.UseVisualStyleBackColor = true;
+            this.ZeroButton.Click += new System.EventHandler(this.ZeroButton_Click);
+            // 
+            // OneButton
+            // 
+            this.OneButton.Location = new System.Drawing.Point(84, 65);
+            this.OneButton.Name = "OneButton";
+            this.OneButton.Size = new System.Drawing.Size(75, 23);
+            this.OneButton.TabIndex = 1;
+            this.OneButton.Text = "1";
+            this.OneButton.UseVisualStyleBackColor = true;
+            this.OneButton.Click += new System.EventHandler(this.OneButton_Click);
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Location = new System.Drawing.Point(165, 65);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(75, 23);
+            this.DeleteButton.TabIndex = 3;
+            this.DeleteButton.Text = "Delete";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
             // 
             // NextButton
             // 
-            this.NextButton.Location = new System.Drawing.Point(84, 212);
+            this.NextButton.Location = new System.Drawing.Point(84, 94);
             this.NextButton.Name = "NextButton";
             this.NextButton.Size = new System.Drawing.Size(75, 23);
-            this.NextButton.TabIndex = 8;
+            this.NextButton.TabIndex = 5;
             this.NextButton.Text = "Next";
             this.NextButton.UseVisualStyleBackColor = true;
             this.NextButton.Click += new System.EventHandler(this.NextButton_Click);
             // 
-            // StartButton
+            // PreviousButton
             // 
-            this.StartButton.Location = new System.Drawing.Point(165, 212);
-            this.StartButton.Name = "StartButton";
-            this.StartButton.Size = new System.Drawing.Size(75, 23);
-            this.StartButton.TabIndex = 7;
-            this.StartButton.Text = "Start";
-            this.StartButton.UseVisualStyleBackColor = true;
-            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            this.PreviousButton.Location = new System.Drawing.Point(3, 94);
+            this.PreviousButton.Name = "PreviousButton";
+            this.PreviousButton.Size = new System.Drawing.Size(75, 23);
+            this.PreviousButton.TabIndex = 4;
+            this.PreviousButton.Text = "Prev";
+            this.PreviousButton.UseVisualStyleBackColor = true;
+            this.PreviousButton.Click += new System.EventHandler(this.PreviousButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(360, 23);
+            this.textBox1.TabIndex = 6;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Location = new System.Drawing.Point(6, 18);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(105, 45);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "pos: 0";
+            this.label1.Size = new System.Drawing.Size(40, 15);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "2進数:";
             // 
-            // UserControl1
+            // textBox2
+            // 
+            this.textBox2.AllowDrop = true;
+            this.textBox2.Location = new System.Drawing.Point(3, 138);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(360, 23);
+            this.textBox2.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 15);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "10進数:";
+            // 
+            // UserControl5
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.PreviousButton);
-            this.Controls.Add(this.NextButton);
-            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label1);
-            this.Name = "UserControl1";
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.NextButton);
+            this.Controls.Add(this.PreviousButton);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.OneButton);
+            this.Controls.Add(this.ZeroButton);
+            this.Name = "UserControl5";
             this.Size = new System.Drawing.Size(366, 238);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -91,9 +146,14 @@
 
         #endregion
 
-        private Button PreviousButton;
+        private Button ZeroButton;
+        private Button OneButton;
+        private Button DeleteButton;
         private Button NextButton;
-        private Button StartButton;
+        private Button PreviousButton;
+        private TextBox textBox1;
         private Label label1;
+        private TextBox textBox2;
+        private Label label2;
     }
 }
--- StepByStepVisualizationWinForm/Models/Control1.Model.cs
+++ StepByStepVisualizationWinForm/Models/Control1.Model.cs
@@ -1,20 +1,56 @@
-﻿namespace StepByStepVisualizationWinForm.Control1;
+﻿using System.Numerics;
 
-internal class Model
+namespace StepByStepVisualizationWinForm.Control1;
+
+internal record struct Model(string Text, BigInteger Value)
 {
-    private Random _rand = new Random(1000000001);
-    public IEnumerator<Operation> Move(int n)
+    public static Model InitialState => new Model() { Text = "", Value = 0 };
+}
+
+internal class BinaryToDecimalConverter
+{
+    public static Model ChangeState(Model model, Operation operation, bool toNextState) => toNextState switch
     {
-        int prev = 0;
-        for (int i = 1; i <= n; i++)
+        true => NextState(model, operation),
+        false => PrevState(model, operation),
+    };
+
+    public static Model NextState(Model model, Operation operation)
+    {
+        var (text, value) = model;
+        var (op, number) = operation;
+
+        var (nextText, nextValue) = op switch
         {
-            int next = _rand.Next(100);
-            yield return new Operation(OperationType.Move, prev, next);
-            prev = next;
-        }
+            OperationType.Append => (text + number, (value << 1) + int.Parse(number.ToString())),
+            OperationType.Delete => (text[..^1], value >> 1),
+            _ => throw new ArgumentException(nameof(operation)),
+        };
+
+        return new(nextText, nextValue);
+    }
+
+    public static Model PrevState(Model model, Operation operation)
+    {
+        var (text, value) = model;
+        var (op, number) = operation;
+
+        var (nextText, nextValue) = op switch
+        {
+            OperationType.Append => (text[..^1], value >> 1),
+            OperationType.Delete => (text + number, (value << 1) + int.Parse(number.ToString())),
+            _ => throw new ArgumentException(nameof(operation)),
+        };
+
+        return new(nextText, nextValue);
     }
 }
 
-internal enum OperationType { None, Move }
+internal enum OperationType
+{
+    Append,
+    Delete,
+}
 
-internal record Operation(OperationType OperationType, int From , int To);
+internal record struct Operation(OperationType OperationType, char Char);
+
